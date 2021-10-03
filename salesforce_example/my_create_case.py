c70fa54246a8792333b09ff6ad0a3235b33f5e2b
Maycon Carlete <maycon.carlete@gmail.com>
2021-10-03 18:34:33 -0300

    feat: call right function

--- salesforce_example/my_create_case.py
+++ salesforce_example/my_create_case.py
@@ -4,13 +4,13 @@ from application.validations.check_field_instance import CheckFieldInstance
 
 
 def create_case_in_salesforce_account(case_requirements: CaseRequirements):
-    validate_case_title = CheckFieldInstance(str)
-    case_requirements.case_title = case_requirements.case_title if validate_case_title.validate(case_requirements) else None
-    # case_requirements.record_type_case_name = check_field_instance(case_requirements.record_type_case_name, str)
+    check_field_instance = CheckFieldInstance(str)
+    case_requirements.case_title = case_requirements.case_title if check_field_instance.validate(case_requirements.case_title) else None
+    case_requirements.record_type_case_name = case_requirements.record_type_case_name if check_field_instance.validate(case_requirements.record_type_case_name) else None
 
     # VERIFY COMPANY IN API ADMIN
     # verify
-
+    return case_requirements.case_title
 
 def check_field_instance(field: str, type: Type):
     if isinstance(field, type):
@@ -20,8 +20,12 @@ def check_field_instance(field: str, type: Type):
 
 
 if __name__ == '__main__':
-    payload = {
-        'teste': 'a'
-    }
-    response = check_field_instance(payload['teste'], str)
+    salesforce_case = CaseRequirements(
+        company_id='abc_123',
+        document_number='3321',
+        description='description',
+        case_title='case_title',
+        record_type_case_name='record_type_case_name'
+    )
+    response = create_case_in_salesforce_account(salesforce_case)
     print(response)
